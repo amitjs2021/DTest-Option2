988af24a8dda0a34fb662c89a9ad94457ed803a5
Amit Sharma <amitsharma@192-168-1-101.tpgi.com.au>
2021-09-20 08:54:15 +1000

    updates

--- app/hcard.js
+++ app/hcard.js
@@ -1,4 +1,4 @@
-/* Due to the hCard component being pre-build without the Rect dependency, this script is used to
+/* 
  * re-export the component with React in the global scope.
  */
 import React from 'react';
