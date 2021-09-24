2d50ce37ce0206688a648da4121787854f34b706
Amit Sharma <amitsharma@192-168-1-101.tpgi.com.au>
2021-09-24 23:41:45 +1000

    no script and connection speed check

--- src/App.js
+++ src/App.js
@@ -2,21 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = React.useState(0);
 
-  const increment = () => {
-    setCount(count + 1);
-  };
-
-  const decrement = () => {
-    setCount(count - 1);
-  };
 
   return (
     <>
-      <p>{count}</p>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <h1>hello</h1>
     </>
   );
 }
