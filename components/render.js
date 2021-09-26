b6cd2e234420a9f13d867b71187efabdd4279f1d
Amit Sharma <amitsharma@192-168-1-101.tpgi.com.au>
2021-09-26 16:24:26 +1000

    new session changes

--- components/render.js
+++ components/render.js
@@ -22,7 +22,7 @@ import { find } from './db';
 export default (req, res) => {
   console.log("req.session :: ", req.sessionID)
   return (
-    find(req.cookies.user)
+    find(req.sessionID)
       .then(state => {
         const content = renderToStaticMarkup(
           <HTMLDocument
